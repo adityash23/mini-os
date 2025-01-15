6b16af4a966085654846fcf377470897ad6c0ffe
adityash23 <aditya3242@gmail.com>
2025-01-15 18:27:40 -0500

    exception handling implemented

--- common.c
+++ common.c
@@ -64,3 +64,47 @@ void printf(const char *fmt, ...) {
 end :
 	va_end(vargs);
 }
+
+// copy 'n' bytes from src to dst
+void *memcpy(void *dst, const void *src, size_t n) {
+	uint8_t *d = (uint8_t *) dst;
+	const uint8_t *s = (const uint8_t *) src;
+	while (n--) {
+		*d++ = *s++;
+	}
+	return dst;
+}
+
+void *memset(void *buf, char c, size_t n) {
+	uint8_t *p = (uint8_t *) buf;
+	while (n--) {
+		*p++ = c;
+		// expanded version of above expression
+		/*
+		 *p = c;
+		 p = p + 1;
+		 */
+	}
+	return buf;
+}
+
+char *strcpy(char *dst, const char *src) {
+	char *d = dst;
+	while (*src) {
+		*d++ = *src++; // *d = *src and then increment both
+	}
+	*d = '\0';
+	return dst;
+}
+
+int strcmp(const char *s1, const char *s2) {
+	while (*s1 && *s2) {
+		if (*s1 != *s2) {
+			break;
+		}
+		s1++;
+		s2++;
+	}
+
+	return *(unsigned char *)s1 - *(unsigned char *)s2;
+}
